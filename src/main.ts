466ad1349c87d3f2358d7c54001776e7cdfc7c56
Saad-Abbasi <sabbasi782@gmail.com>
2021-06-04 20:16:35 +0500

    console off

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  console.log = function() {}
-  console.error = function () {}
+  
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  console.log = function() {}
+  console.error = function () {}
   
